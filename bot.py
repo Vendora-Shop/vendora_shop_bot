b43a6dfb4bb31dc1fd1edd1bc98be4e7c2b03070
Vendora-Shop <bestserverscs@gmail.com>
2026-05-02 17:55:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,15 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from database import create_tables
-from shop_handlers import router as shop_router
 from admin_handlers import router as admin_router
+from shop_handlers import router as shop_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# חשוב מאוד: קודם החנות, אחרי זה האדמין
-dp.include_router(shop_router)
+# חשוב: קודם אדמין, אחר כך חנות
 dp.include_router(admin_router)
+dp.include_router(shop_router)
 
 
 async def main():
