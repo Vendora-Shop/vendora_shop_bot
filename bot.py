e77d05e8c67afb5cc3872722b5365c71827823cd
Vendora-Shop <bestserverscs@gmail.com>
2026-05-02 17:02:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-
 from config import BOT_TOKEN
 from database import create_tables
 from admin_handlers import router as admin_router
+from shop_handlers import router as shop_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(admin_router)
+dp.include_router(shop_router)
 
 
 async def main():
