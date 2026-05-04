46b0eb34c562fe4ba5b950c1a790e1da420f4ea7
Vendora-Shop <bestserverscs@gmail.com>
2026-05-04 14:05:01 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -41,12 +41,12 @@ def create_invoice_pdf(order):
     LINE_W = 1
     BOX_W = 2
 
-    label_font = ImageFont.truetype(FONT_PATH, 30)
-    value_font = ImageFont.truetype(FONT_PATH, 26)
-    address_font = ImageFont.truetype(FONT_PATH, 26)
-    section_font = ImageFont.truetype(FONT_PATH, 36)
-    table_font = ImageFont.truetype(FONT_PATH, 36)
-    total_label_font = ImageFont.truetype(FONT_PATH, 36)
+    label_font = ImageFont.truetype(FONT_PATH, 28)
+    value_font = ImageFont.truetype(FONT_PATH, 22)
+    address_font = ImageFont.truetype(FONT_PATH, 28)
+    section_font = ImageFont.truetype(FONT_PATH, 32)
+    table_font = ImageFont.truetype(FONT_PATH, 28)
+    total_label_font = ImageFont.truetype(FONT_PATH, 34)
     total_font = ImageFont.truetype(FONT_PATH, 30)
     note_font = ImageFont.truetype(FONT_PATH, 26)
 
