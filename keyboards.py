8067f186a30ffc3408d775c594fcda469638ddfd
Vendora-Shop <bestserverscs@gmail.com>
2026-05-05 17:15:07 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -14,9 +14,9 @@ def main_keyboard():
 def admin_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📊 מצב העסק"), KeyboardButton(text="📅 סטטיסטיקה לפי תאריך")],
             [KeyboardButton(text="🧾 הזמנות אחרונות"), KeyboardButton(text="🆕 הזמנות חדשות")],
             [KeyboardButton(text="🔎 חפש הזמנה"), KeyboardButton(text="📞 חפש לפי טלפון")],
+            [KeyboardButton(text="📊 מצב העסק"), KeyboardButton(text="📅 סטטיסטיקה לפי תאריך")],
             [KeyboardButton(text="📊 סטטיסטיקה יומית"), KeyboardButton(text="🔄 עדכן סטטוס הזמנה")],
             [KeyboardButton(text="➕ הוסף מוצר"), KeyboardButton(text="📦 רשימת מוצרים")],
             [KeyboardButton(text="✏️ שנה מחיר"), KeyboardButton(text="📝 שנה תיאור")],
